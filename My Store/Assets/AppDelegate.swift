a6c6424add6e26608c526ba9eb0b63417e7ba34a
ShashiKant_B <shashibhadke22@gmail.com>
2019-08-17 13:14:28 +0530

    Add files via upload
    Initial setUp

---
+++ My Store/Assets/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  My Store
+//
+//  Created by Shashikant Bhadke on 17/08/19.
+//  Copyright © 2019 Shashikant Bhadke. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+} //class
---
+++ My Store/Controller/LoginVC.swift
@@ -0,0 +1,17 @@
+//
+//  LoginVC.swift
+//  My Store
+//
+//  Created by Shashikant Bhadke on 17/08/19.
+//  Copyright © 2019 Shashikant Bhadke. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
